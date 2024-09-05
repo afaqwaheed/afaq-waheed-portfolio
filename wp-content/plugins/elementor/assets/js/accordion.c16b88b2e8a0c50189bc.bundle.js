e1852a6093d181fd93cebfc2a3ea22a2bad12cc6
Afaq Waheed <81997720+afaqwaheed@users.noreply.github.com>
2024-09-05 06:47:59 +0500

    new update in portfolio personal branding agency

--- wp-content/plugins/elementor/assets/js/accordion.c16b88b2e8a0c50189bc.bundle.js
+++ wp-content/plugins/elementor/assets/js/accordion.c16b88b2e8a0c50189bc.bundle.js
@@ -1,4 +1,4 @@
-/*! elementor - v3.20.0 - 10-04-2024 */
+/*! elementor - v3.23.0 - 05-08-2024 */
 "use strict";
 (self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["accordion"],{
 
